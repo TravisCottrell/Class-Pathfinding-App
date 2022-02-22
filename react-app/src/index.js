f5f6196275e131ff388512f4c7a61aabbd01cf0c
TravisCottrell <tcottre4@kent.edu>
2022-02-22 17:42:03 -0500

    Updates

--- react-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-//import reportWebVitals from './reportWebVitals';
-import Pathing from "./components/pathingApp";
-
-ReactDOM.render(<Pathing />, document.getElementById('root'));
-
-
